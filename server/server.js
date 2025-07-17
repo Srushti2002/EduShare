2f7bca3fce63aff3378f77fb1b11a39a9dda5c81
Srushti <srushtideshmukh54@gmail.com>
2025-07-17 22:41:28 +0530

    Client port changed

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:4173'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
