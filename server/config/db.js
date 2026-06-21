d928f51571760d75c67d0f57106b84035f9712b8
Srushti2002 <srushtideshmukh54@gmail.com>
2026-06-21 15:19:54 +0530

    removed unused string URL

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 const mongoURL = process.env.MONGODB_URL;
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
 
